1377439eb68e940ea3064a088de22a28638b2717
Jose Javier <jose.javier@pucp.edu.pe>
2024-01-02 22:31:45 -0500

    feat(unsplash): integrate using unsplash-js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    UNSPLASH_KEY: process.env.UNSPLASH_KEY,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
---
+++ src/services/unsplash-api.ts
@@ -0,0 +1,5 @@
+import { createApi } from 'unsplash-js';
+
+export const unsplashClient = createApi({
+  accessKey: process.env.UNSPLASH_KEY,
+});
